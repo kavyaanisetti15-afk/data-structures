0046fc71a02300553200df79327c0034d476b690
Kavyanjali Anisetti <kavyaanisetti15@gmail.com>
2026-02-10 14:49:41 +0530

    Add files via upload

---
+++ program 10.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct student
+{
+    int roll;
+    char name[20];
+    struct dob
+    {
+        int day, month, year;
+    } d;
+} s1;
+
+int main()
+{
+    printf("\nEnter student roll number: ");
+    scanf("%d", &s1.roll);
+
+    printf("\nEnter student name: ");
+    scanf("%s", s1.name);
+
+    printf("\nEnter student date of birth (dd mm yy): ");
+    scanf("%d %d %d", &s1.d.day, &s1.d.month, &s1.d.year);
+
+    printf("\nStudent roll = %d", s1.roll);
+    printf("\nStudent name = %s", s1.name);
+    printf("\nStudent date of birth = %d-%d-%d",
+           s1.d.day, s1.d.month, s1.d.year);
+
+    return 0;
+}
